886cd46c7fea6be7ed676a0e9bbec8f72907e897
Adam Roszyk <funkylogic.studio@gmail.com>
2025-03-19 11:55:18 +0100

    Initial Commit

---
+++ HairStyle/HairStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HairStyleApp.swift
+//  HairStyle
+//
+//  Created by Adam Roszyk on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HairStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
